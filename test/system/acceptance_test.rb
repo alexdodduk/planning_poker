a7b0e9195391ece3c971893391693c041da90cc0
Alex Dodd <alex.dodd@altmetric.com>
2025-04-11 16:20:23 +0100

    Add more acceptance tests

--- test/system/acceptance_test.rb
+++ test/system/acceptance_test.rb
@@ -8,14 +8,14 @@ class AcceptanceTest < ApplicationSystemTestCase
   end
 
   test "Entering a new room as a player" do
-    using_session("host's session") do
-      visit root_path
-      click_on "Create a room"
-      assert_text 'You are the host'
-    end
+    # host's session
+    visit root_path
+    click_on "Create a room"
+    assert_text 'You are the host'
+    room_url = current_path
 
     using_session("player's session") do
-      visit room_path 1
+      visit room_url
       assert_text 'Waiting for players to join'
 
       fill_in 'player_name', with: 'Alice'
@@ -26,14 +26,14 @@ class AcceptanceTest < ApplicationSystemTestCase
   end
 
   test "Multiple players" do
-    using_session("host's session") do
-      visit root_path
-      click_on "Create a room"
-      assert_text 'You are the host'
-    end
+    # host's session
+    visit root_path
+    click_on "Create a room"
+    assert_text 'You are the host'
+    room_url = current_path
 
     using_session("player 1's session") do
-      visit room_path 1
+      visit room_url
       assert_text 'Waiting for players to join'
 
       fill_in 'player_name', with: 'Alice'
@@ -43,7 +43,7 @@ class AcceptanceTest < ApplicationSystemTestCase
     end
 
     using_session("player 2's session") do
-      visit room_path 1
+      visit room_url
 
       fill_in 'player_name', with: 'Bob'
       click_button 'Join room'
@@ -54,14 +54,14 @@ class AcceptanceTest < ApplicationSystemTestCase
   end
 
   test "Visiting room 2 after joining room 1" do
-    using_session("host's session") do
-      visit root_path
-      click_on "Create a room"
-      assert_text 'You are the host'
-    end
+    # host's session
+    visit root_path
+    click_on "Create a room"
+    assert_text 'You are the host'
+    room_url = current_path
 
     using_session("player's session") do
-      visit room_path 1
+      visit room_url
       assert_text 'Waiting for players to join'
       assert_text 'Name'
 
@@ -71,16 +71,66 @@ class AcceptanceTest < ApplicationSystemTestCase
       assert_text 'Alice'
     end
 
-    using_session("host's session") do
-      visit root_path
-      click_on "Create a room"
-      assert_text 'You are the host'
-    end
+    # host's session
+    visit root_path
+    click_on "Create a room"
+    assert_text 'You are the host'
 
     using_session("player's session") do
       visit room_path 2
       assert_text 'Waiting for players to join'
       assert_text 'Name'
     end
+  end
+
+  test "Revealing scores and resetting room" do
+    # host's session
+    visit root_path
+    click_on "Create a room"
+    assert_text 'You are the host'
+    room_url = current_path
+
+    using_session("player 1's session") do
+      visit room_url
+      assert_text 'Waiting for players to join'
+
+      fill_in 'player_name', with: 'Alice'
+      click_button 'Join room'
+
+      assert_text 'Score (Alice)'
+
+      # Cast a vote
+      find("label[for='player_score_1']").click
+      click_on "Set score"
+    end
+
+    # Host reveals votes
+    click_on "Reveal"
+    assert_selector ".card.revealed", text: "1"
+
+    # Host resets votes
+    click_on "Reset"
+    assert_no_selector ".revealed"
+  end
+
+  test "host can end the game" do
+    # host's session
+    visit root_path
+    click_on "Create a room"
+    assert_text 'You are the host'
+    room_url = current_path
+
+    using_session("player 1's session") do
+      visit room_url
+      fill_in 'player_name', with: 'Alice'
+      click_button 'Join room'
+
+      find("legend", text: "Score (Alice)")
+    end
+
+    # host's session
+    assert_text "Alice"
+    click_on "End game"
+    find("#notice", text: "Game has ended, thanks for playing!")
   end
 end
