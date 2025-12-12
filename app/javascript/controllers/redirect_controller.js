dd9cd0748ee45acb0584df8f672e5deec5d23ad4
Alex Dodd <alex.dodd.uk@gmail.com>
2025-12-12 13:49:35 +0000

    Explain how the redirect Stimulus controller works

--- app/javascript/controllers/redirect_controller.js
+++ app/javascript/controllers/redirect_controller.js
@@ -1,9 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  // todo: explain what is going on here
+  // This declares a Stimulus "value"
+  // The div that connects to this controller also has a data-redirect-url-value attribute
+  // whose value is the URL to redirect to.
+  // This value is automatically mapped to this.urlValue by Stimulus
+  // due to the naming convention: data-[controller]-[value-name]-value.
   static values = { url: String }
   connect() {
+    // accesses the `url` field in the controller's `values` object,
+    // which contains the URL specified in the data attribute,
+    // then immediately redirects the browser to that URL
     window.location.href = this.urlValue
   }
 }
